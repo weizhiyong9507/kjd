1c898453927be2d591e1fe6034a9d525fd168259
weizhiyong9507 <zhiyong_wei9507@163.com>
2019-02-11 22:04:50 +0800

    qqq

---
+++ js/index.js
@@ -0,0 +1,11 @@
+let $coverBox = $('.coverBox'),
+    $myResume = $('.myResume');
+
+setTimeout(()=>{
+    $coverBox.css({
+        display:'none'
+    })
+    $myResume.css({
+        display:'block'
+    });
+},2000)
